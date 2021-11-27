d1b34ecc433a72d771b7c48d465dbc33ed1b1f27
agumusay <agumusay@protonmail.comm>
2021-11-27 19:47:42 +0100

    ui-storybook-chromatic

--- src/helpers/Field.test.ts
+++ src/helpers/Field.test.ts
@@ -1,39 +1,40 @@
-import { emptyFieldGenerator, CellState, fieldGenerator } from './Field';
+import { generateFieldWithDefaultState, fieldGenerator, CellState, Cell } from './Field';
+
 const { empty, bomb, hidden } = CellState;
 
+const cellWithBombFilter = (cell: Cell) => cell === bomb;
+
 describe('Field Generator', () => {
-	describe('emptyFieldGenerator Tests', () => {
-		it('2×2', () => {
-			expect(emptyFieldGenerator(2)).toStrictEqual([
+	describe('emptyFieldGenerator tests', () => {
+		it('2x2', () => {
+			expect(generateFieldWithDefaultState(2)).toStrictEqual([
 				[empty, empty],
 				[empty, empty],
 			]);
 		});
-		it('3×3', () => {
-			expect(emptyFieldGenerator(3)).toStrictEqual([
+		it('3x3', () =>
+			expect(generateFieldWithDefaultState(3)).toStrictEqual([
 				[empty, empty, empty],
 				[empty, empty, empty],
 				[empty, empty, empty],
-			]);
-		});
-		it('3×3 with hidden state', () => {
-			expect(emptyFieldGenerator(3, hidden)).toStrictEqual([
+			]));
+		it('3x3 with hidden state', () =>
+			expect(generateFieldWithDefaultState(3, hidden)).toStrictEqual([
 				[hidden, hidden, hidden],
 				[hidden, hidden, hidden],
 				[hidden, hidden, hidden],
-			]);
-		});
+			]));
 	});
 	describe('Simple cases', () => {
-		it('has wrong density', () => {
-			const errorText = 'Probability should be between 0 and 1';
+		it('Wrong dencity', () => {
+			const errorText = 'Probability must be between 0 and 1';
 			expect(() => fieldGenerator(1, -1)).toThrow(errorText);
 			expect(() => fieldGenerator(1, 2)).toThrow(errorText);
 		});
-		it("'s smallest possible field without mine", () => {
+		it('Smallest possible field without mine', () => {
 			expect(fieldGenerator(1, 0)).toStrictEqual([[empty]]);
 		});
-		it('s Biggest field without mine', () => {
+		it('Big field without mine', () => {
 			expect(fieldGenerator(10, 0)).toStrictEqual([
 				[empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
 				[empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
@@ -47,31 +48,37 @@ describe('Field Generator', () => {
 				[empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
 			]);
 		});
-		it("'s smallest possible field with mine", () => {
+		it('Smallest possible field with mine', () => {
 			expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]]);
 		});
-		it('has 2×2 field with mines', () => {
+		it('2x2 field with mines', () => {
 			expect(fieldGenerator(2, 1)).toStrictEqual([
 				[bomb, bomb],
 				[bomb, bomb],
 			]);
 		});
-		it('has 2×2 field with 50% probablity', () => {
+		it('2x2 field with 50% probability', () => {
 			const field = fieldGenerator(2, 0.5);
 			const flatField = field.flat();
 
-			const cellsWithBombs = flatField.filter(cell => cell === bomb);
-			const emptyCells = flatField.filter(cell => cell === empty);
+			const cellsWithBombs = flatField.filter(cellWithBombFilter);
+			const emptyCells = flatField.filter((cell: Cell) => cell === 2);
 
-			expect(emptyCells).toHaveLength(2);
 			expect(cellsWithBombs).toHaveLength(2);
+			expect(emptyCells).toHaveLength(2);
+		});
+		it('Real game field size = 10x10 with 1/4 mined cells (25 mines)', () => {
+			const size = 10;
+			const mines = 25;
 
-			expect(fieldGenerator(4, 0.5)).toStrictEqual([
-				[bomb, bomb, bomb, bomb],
-				[bomb, bomb, bomb, bomb],
-				[empty, empty, empty, empty],
-				[empty, empty, empty, empty],
-			]);
+			const probability = mines / (size * size);
+			const field = fieldGenerator(size, probability);
+
+			const flatField = field.flat();
+
+			expect([...field[0], ...field[1]].join('')).not.toBe('99999999999999999999');
+
+			expect(flatField.filter(cellWithBombFilter)).toHaveLength(mines);
 		});
 	});
 });
--- src/helpers/Field.ts
+++ src/helpers/Field.ts
@@ -1,3 +1,5 @@
+import { incrementNeighbours } from './CellsManipulator';
+
 export type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type Field = Cell[][];
 export type Coords = [number, number];
@@ -6,34 +8,34 @@ export const CellState: Record<string, Cell> = {
 	empty: 0,
 	bomb: 9,
 	hidden: 10,
-	mark: 11,
-	weakMark: 12,
+	flag: 11,
+	weakFlag: 12,
 };
 
-export const emptyFieldGenerator = (size: number, state: Cell = CellState.empty): Field =>
+export const generateFieldWithDefaultState = (size: number, state: Cell = CellState.empty): Field =>
 	new Array(size).fill(null).map(() => new Array(size).fill(state));
 
 export const fieldGenerator = (size: number, probability: number): Field => {
-	if (probability > 1 || probability < 0) {
-		throw new Error('Probability should be between 0 and 1');
+	if (probability < 0 || probability > 1) {
+		throw new Error('Probability must be between 0 and 1');
 	}
+
 	let unprocessedCells = size * size;
 	let restCellsWithBombs = unprocessedCells * probability;
 
-	const result: Field = emptyFieldGenerator(size);
+	const result: Field = generateFieldWithDefaultState(size);
 
-	for (let i = 0; i < size; i++) {
-		for (let j = 0; j < size; j++) {
-			if (restCellsWithBombs === 0) {
-				return result;
-			}
-			if (restCellsWithBombs / unprocessedCells > 0) {
-				result[i][j] = CellState.bomb;
+	// Stryker disable next-line EqualityOperator
+	for (let y = 0; y < size; y++) {
+		for (let x = 0; x < size; x++) {
+			// Stryker disable next-line EqualityOperator
+			if (restCellsWithBombs / unprocessedCells > Math.random()) {
+				result[y][x] = CellState.bomb;
+				incrementNeighbours([y, x], result);
 				restCellsWithBombs--;
 			}
 			unprocessedCells--;
 		}
 	}
-
 	return result;
 };
